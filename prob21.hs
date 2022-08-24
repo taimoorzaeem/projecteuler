cf8e5b2fb0f99bc7c3b036392e601846234dda7b
Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2022-08-24 14:03:20 +0500

    Added problem 21.

---
+++ prob21.hs
@@ -0,0 +1,15 @@
+module AmicableNums where
+
+
+-- An VERY inefficient solution
+
+getDivisors :: Int -> [Int]
+getDivisors n = [ x | x <- [1..(div n 2)], mod n x == 0 ]
+
+
+getAmicableNums :: Int -> [Int]
+getAmicableNums n = [ x | x <- [1..n], 
+                        x == 
+                        sum (getDivisors (sum (getDivisors x)))]
+
+-- To Run: Load the module in ghci and type "getAmicableNums 10000".
